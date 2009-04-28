c8f451b12034ca249c4c4a822f2ac007f6019404
mattlevine <mattlevine@50f7a5e4-a141-4b55-9860-49daa24b3293>
2009-04-28 16:53:02 +0000

    adding new version of fckeditor
    git-svn-id: http://svn.blueriver.com/mura/trunk/www@181 50f7a5e4-a141-4b55-9860-49daa24b3293

---
+++ fckeditor/editor/plugins/SelectComponent/lang/en.js
@@ -0,0 +1,20 @@
+/*
+ * FCKeditor - The text editor for internet
+ * Copyright (C) 2003-2005 Frederico Caldeira Knabben
+ * 
+ * Licensed under the terms of the GNU Lesser General Public License:
+ * 		http://www.opensource.org/licenses/lgpl-license.php
+ * 
+ * For further information visit:
+ * 		http://www.fckeditor.net/
+ * 
+ * File Name: en.js
+ * 	Placholder English language file.
+ * 
+ * File Authors:
+ * 		Frederico Caldeira Knabben (fredck@fckeditor.net)
+ */
+FCKLang.SelectComponentBtn			= 'Insert Component' ;
+FCKLang.SelectComponentDlgTitle		= 'Insert Component' ;
+FCKLang.SelectComponentDlgName		= 'Insert Component' ;
+
