3d56ea787c400c6c255f6aa68be395b60d988a0a
mattlevine <mattlevine@50f7a5e4-a141-4b55-9860-49daa24b3293>
2010-07-26 17:15:22 +0000

    1. Adding a sort of tcontent.created desc to the related content query.
    git-svn-id: http://svn.blueriver.com/mura/trunk/www@2578 50f7a5e4-a141-4b55-9860-49daa24b3293

--- admin/js/jquery/jquery-ui-i18n.js
+++ admin/js/jquery/jquery-ui-i18n.js
@@ -1174,3 +1174,25 @@ jQuery(function($){
 		yearSuffix: '年'};
 	$.datepicker.setDefaults($.datepicker.regional['zh-TW']);
 });
+/* Added by Blue River */
+jQuery(function($){
+	$.datepicker.regional['en-CA'] = {
+		closeText: 'Done',
+		prevText: 'Prev',
+		nextText: 'Next',
+		currentText: 'Today',
+		monthNames: ['January','February','March','April','May','June',
+		'July','August','September','October','November','December'],
+		monthNamesShort: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+		'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+		dayNames: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+		dayNamesShort: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+		dayNamesMin: ['Su','Mo','Tu','We','Th','Fr','Sa'],
+		weekHeader: 'Wk',
+		dateFormat: 'dd/mm/yy',
+		firstDay: 1,
+		isRTL: false,
+		showMonthAfterYear: false,
+		yearSuffix: ''};
+	$.datepicker.setDefaults($.datepicker.regional['en-CA']);
+});
