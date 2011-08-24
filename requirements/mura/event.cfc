MasaCMS/MasaCMS

commit 0a458066d724ea1174e48899aa0eca5db28833d6

1. Adding logic so that all event object have the data provided in the init as well as  keys from the form an url scopes

requirements/mura/event.cfc
@@ -48,6 +48,8 @@ to your own modified versions of Mura CMS.
 	<cfargument name="data"  type="any" default="#structNew()#">
 	
 	<cfset variables.event=arguments.data />
+	<cfset structAppend(variables.event,form,false)/>
+	<cfset structAppend(variables.event,url,false)/>
 	
 	<cfif len(getValue('siteid')) and application.settingsManager.siteExists(getValue('siteid'))>
 		<cfset loadSiteRelatedObjects()/>

commit 2703d4156c10a839d285c6f6b6fb0e858b1b6b61

Added conditional logic to only append form scope to event, if form scope defined. Previously this caused all WSDL requests to fail, as this logic is called from onRequestStart event, which is also processed for WSDL requests, in which the form scope is not defined. This issue was introduced in [0a458066d724ea1174e4]

requirements/mura/event.cfc
@@ -48,7 +48,7 @@ to your own modified versions of Mura CMS.
 	<cfargument name="data"  type="any" default="#structNew()#">
 	
 	<cfset variables.event=arguments.data />
-	<cfset structAppend(variables.event,form,false)/>
+	<cfif isdefined("form")><cfset structAppend(variables.event,form,false)/></cfif>
 	<cfset structAppend(variables.event,url,false)/>
 	
 	<cfif len(getValue('siteid')) and application.settingsManager.siteExists(getValue('siteid'))>