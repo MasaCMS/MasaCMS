17f80db0b902e94a786ea9e5dcd225d2b96905e8
mattlevine <mattlevine@50f7a5e4-a141-4b55-9860-49daa24b3293>
2012-12-31 17:44:06 +0000

    Merge branch 'master' of github.com:blueriver/MuraCMS
    git-svn-id: http://svn.blueriver.com/mura/trunk/www@5195 50f7a5e4-a141-4b55-9860-49daa24b3293

--- default/js/global.js
+++ default/js/global.js
@@ -457,6 +457,31 @@ function submitForm(frm,action,theClass){
 	return false;
 }
 
+function createCookie(name,value,days) {
+	if (days) {
+		var date = new Date();
+		date.setTime(date.getTime()+(days*24*60*60*1000));
+		var expires = "; expires="+date.toGMTString();
+	}
+	else var expires = "";
+	document.cookie = name+"="+value+expires+"; path=/";
+}
+
+function readCookie(name) {
+	var nameEQ = name + "=";
+	var ca = document.cookie.split(';');
+	for(var i=0;i < ca.length;i++) {
+		var c = ca[i];
+		while (c.charAt(0)==' ') c = c.substring(1,c.length);
+		if (c.indexOf(nameEQ) == 0) return unescape(c.substring(nameEQ.length,c.length));
+	}
+	return "";
+}
+
+function eraseCookie(name) {
+	createCookie(name,"",-1);
+}
+
 function addLoadEvent(func) {
    var oldonload = window.onload;
    if (typeof window.onload != 'function') {
