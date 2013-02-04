6de9ce2195754dc67cf9d498986ca9f3a077dae9
mattlevine <mattlevine@50f7a5e4-a141-4b55-9860-49daa24b3293>
2013-02-04 20:10:15 +0000

    Merge branch 'master' of github.com:blueriver/MuraCMS
    git-svn-id: http://svn.blueriver.com/mura/trunk/www@5263 50f7a5e4-a141-4b55-9860-49daa24b3293

--- default/js/global.js
+++ default/js/global.js
@@ -617,6 +617,11 @@ function getHTMLEditorConfig(customConfig) {
 	return htmlEditorConfig;
 }
 
+function extendObject(obj1,obj2){
+	for (var attrname in obj2) { obj1[attrname] = obj2[attrname]; }
+	return obj1;
+}
+
 $(document).ready(setMuraLoginCheck);
 
 //Event.observe(window, 'load', setKeyCheck, false);
