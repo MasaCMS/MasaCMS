945b1b541ec848292e78c824c72ba54535cd2def
Steve Withington <stephenwithington@gmail.com>
2014-01-23 22:20:50 -0600

    new feature: purge deleted comments for super admins only

--- admin/assets/js/comment.js
+++ admin/assets/js/comment.js
@@ -44,15 +44,7 @@
 	modified version; it is your choice whether to do so, or to make such modified version available under the GNU General Public License 
 	version 2 without this exception.  You may, if you choose, apply this exception to your own modified versions of Mura CMS. */
 
-
 var commentManager = {
-
-
-	purgeDeletedComments: function(){
-		var url = './';
-		var pars = 'muraAction=cComments.purgeDeletedComments&siteid=' + siteid + '&cacheid=' + Math.random();
-		$.get(url + "?" + pars, function(){commentManager.submitSearch();});
-	},
 
 	loadSearch: function(values){
 		var url = './';
@@ -259,5 +251,11 @@ var commentManager = {
 			url: './',
 			data: params
 		});
+	},
+
+	purgeDeletedComments: function(){
+		var url = './';
+		var pars = 'muraAction=cComments.purgeDeletedComments&siteid=' + siteid + '&cacheid=' + Math.random();
+		$.get(url + "?" + pars, function(){commentManager.submitSearch();});
 	}
 }
