96de626574123828907b7b50120c7728f8bb5573
Mat Levine <matt.levine@blueriver.com>
2015-01-07 14:25:11 -0800

    Adding js/css loader

--- default/js/global.js
+++ default/js/global.js
@@ -78,6 +78,234 @@ cx.lastIndex=0;if(cx.test(text)){text=text.replace(cx,function(a){return'\\u'+('
 if(/^[\],:{}\s]*$/.test(text.replace(/\\(?:["\\\/bfnrt]|u[0-9a-fA-F]{4})/g,'@').replace(/"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g,']').replace(/(?:^|:|,)(?:\s*\[)+/g,''))){j=eval('('+text+')');return typeof reviver==='function'?walk({'':j},''):j;}
 throw new SyntaxError('JSON.parse');}};}();}
 
+//https://github.com/malko/l.js
+;(function(window, undefined){
+/*
+* script for js/css parallel loading with dependancies management
+* @author Jonathan Gotti < jgotti at jgotti dot net >
+* @licence dual licence mit / gpl
+* @since 2012-04-12
+* @todo add prefetching using text/cache for js files
+* @changelog
+*            - 2014-06-26 - bugfix in css loaded check when hashbang is used
+*            - 2014-05-25 - fallback support rewrite + null id bug correction + minification work
+*            - 2014-05-21 - add cdn fallback support with hashbang url
+*            - 2014-05-22 - add support for relative paths for stylesheets in checkLoaded
+*            - 2014-05-21 - add support for relative paths for scripts in checkLoaded
+*            - 2013-01-25 - add parrallel loading inside single load call
+*            - 2012-06-29 - some minifier optimisations
+*            - 2012-04-20 - now sharp part of url will be used as tag id
+*                         - add options for checking already loaded scripts at load time
+*            - 2012-04-19 - add addAliases method
+* @note coding style is implied by the target usage of this script not my habbits
+*/
+	/** gEval credits goes to my javascript idol John Resig, this is a simplified jQuery.globalEval */
+	var gEval = function(js){ ( window.execScript || function(js){ window[ "eval" ].call(window,js);} )(js); }
+		, isA =  function(a,b){ return a instanceof (b || Array);}
+		//-- some minifier optimisation
+		, D = document
+		, getElementsByTagName = 'getElementsByTagName'
+		, length = 'length'
+		, readyState = 'readyState'
+		, onreadystatechange = 'onreadystatechange'
+		//-- get the current script tag for further evaluation of it's eventual content
+		, scripts = D[getElementsByTagName]("script")
+		, scriptTag = scripts[scripts[length]-1]
+		, script  = scriptTag.innerHTML.replace(/^\s+|\s+$/g,'')
+	;
+	//avoid multiple inclusion to override current loader but allow tag content evaluation
+	if( ! window.ljs ){
+		var checkLoaded = scriptTag.src.match(/checkLoaded/)?1:0
+			//-- keep trace of header as we will make multiple access to it
+			,header  = D[getElementsByTagName]("head")[0] || D.documentElement
+			, urlParse = function(url){
+				var parts={}; // u => url, i => id, f = fallback
+				parts.u = url.replace(/#(=)?([^#]*)?/g,function(m,a,b){ parts[a?'f':'i'] = b; return '';});
+				return parts;
+			}
+			,appendElmt = function(type,attrs,cb){
+				var e = D.createElement(type), i;
+				if( cb ){ //-- this is not intended to be used for link
+					if(e[readyState]){
+						e[onreadystatechange] = function(){
+							if (e[readyState] === "loaded" || e[readyState] === "complete"){
+								e[onreadystatechange] = null;
+								cb();
+							}
+						};
+					}else{
+						e.onload = cb;
+					}
+				}
+				for( i in attrs ){ attrs[i] && (e[i]=attrs[i]); }
+				header.appendChild(e);
+				// return e; // unused at this time so drop it
+			}
+			,load = function(url,cb){
+				if( this.aliases && this.aliases[url] ){
+					var args = this.aliases[url].slice(0);
+					isA(args) || (args=[args]);
+					cb && args.push(cb);
+					return this.load.apply(this,args);
+				}
+				if( isA(url) ){ // parallelized request
+					for( var l=url[length]; l--;){
+						this.load(url[l]);
+					}
+					cb && url.push(cb); // relaunch the dependancie queue
+					return this.load.apply(this,url);
+				}
+				if( url.match(/\.css\b/) ){
+					return this.loadcss(url,cb);
+				}
+				return this.loadjs(url,cb);
+			}
+			,loaded = {}  // will handle already loaded urls
+			,loader  = {
+				aliases:{}
+				,loadjs: function(url,attrs,cb){
+					if(typeof url == 'object'){
+						if(Array.isArray(url)){
+							return loader.load.apply(this, arguments);
+						} else if(typeof attrs === 'function'){
+							cb=attrs;
+							attrs={};
+							url=attrs.href
+						} else if (typeof attrs=='string' || (typeof attrs=='object' && Array.isArray(attrs))) {
+							return loader.load.apply(this, arguments);
+						} else {
+							attrs=url;
+							url=attrs.href;
+							cb=undefined;
+						}
+					} else if (typeof attrs=='function' ) {
+						cb = attrs;
+						attrs = {};
+					} else if (typeof attrs=='string' || (typeof attrs=='object' && Array.isArray(attrs))) {
+						return loader.load.apply(this, arguments);
+					}
+					if(typeof attrs==='undefined'){
+						attrs={};
+					}
+
+					var parts = urlParse(url);	
+					var partToAttrs=[['i','id'],['f','fallback'],['u','src']];
+					
+					for(var i=0;i<partToAttrs.length;i++){
+						var part=partToAttrs[i];
+						if(!(part[1] in attrs) && (part[0] in parts)){
+							attrs[part[1]]=parts[part[0]];
+						}
+					}
+				
+					if(typeof attrs.type === 'undefined'){
+						attrs.type='text/javascript';
+					}
+
+					var finalAttrs={};
+
+					for(var a in attrs){
+						if(a != 'fallback'){
+							finalAttrs[a]=attrs[a];
+						}
+					}
+					
+					finalAttrs.onerror=function(error){
+						if( attrs.fallback ){
+							var c = error.currentTarget;
+							c.parentNode.removeChild(c);
+							finalAttrs.src=attrs.fallback;
+							appendElmt('script',attrs,cb);
+						}
+					};
+					
+
+					if( loaded[finalAttrs.src] === true ){ // already loaded exec cb if any
+						cb && cb();
+						return this;
+					} else if( loaded[finalAttrs.src]!== undefined ){ // already asked for loading we append callback if any else return
+						if( cb ){
+							loaded[finalAttrs.src] = (function(ocb,cb){ return function(){ ocb && ocb(); cb && cb(); }; })(loaded[finalAttrs.src],cb);
+						}
+						return this;
+					}
+					// first time we ask this script
+					loaded[finalAttrs.src] = (function(cb){ return function(){loaded[finalAttrs.src]=true; cb && cb();};})(cb);
+					cb = function(){ loaded[url](); };
+					appendElmt('script',finalAttrs,cb);
+					return this;
+				}
+				,loadcss: function(url,attrs,cb){
+
+					if(typeof url == 'object'){
+						if(Array.isArray(url)){
+							return loader.load.apply(this, arguments);
+						} else if(typeof attrs === 'function'){
+							cb=attrs;
+							attrs=url;
+							url=attrs.href
+						} else if (typeof attrs=='string' || (typeof attrs=='object' && Array.isArray(attrs))) {
+							return loader.load.apply(this, arguments);
+						} else {
+							attrs=url;
+							url=attrs.href;
+							cb=undefined;
+						}
+					}	else if (typeof attrs=='function' ) {
+						cb = attrs;
+						attrs = {};
+					} else if (typeof attrs=='string' || (typeof attrs=='object' && Array.isArray(attrs))) {
+						return loader.load.apply(this, arguments);
+					}
+					
+					var parts = urlParse(url);
+					parts={type:'text/css',rel:'stylesheet',href:url,id:parts.i}
+
+					if(typeof attrs !=='undefined'){
+						for(var a in attrs){
+							parts[a]=attrs[a];
+						}
+					}
+
+					loaded[parts.href] || appendElmt('link',parts);
+					loaded[parts.href] = true;
+					cb && cb();
+					return this;
+				}
+				,load: function(){
+					var argv=arguments,argc = argv[length];
+					if( argc === 1 && isA(argv[0],Function) ){
+						argv[0]();
+						return this;
+					}
+					load.call(this,argv[0], argc <= 1 ? undefined : function(){ loader.load.apply(loader,[].slice.call(argv,1));} );
+					return this;
+				}
+				,addAliases:function(aliases){
+					for(var i in aliases ){
+						this.aliases[i]= isA(aliases[i]) ? aliases[i].slice(0) : aliases[i];
+					}
+					return this;
+				}
+			}
+		;
+		if( checkLoaded ){
+			var i,l,links,url;
+			for(i=0,l=scripts[length];i<l;i++){
+				(url = scripts[i].getAttribute('src')) && (loaded[url.replace(/#.*$/,'')] = true);
+			}
+			links = D[getElementsByTagName]('link');
+			for(i=0,l=links[length];i<l;i++){
+				(links[i].rel==='stylesheet' || links[i].type==='text/css') && (loaded[links[i].getAttribute('href').replace(/#.*$/,'')]=true);
+			}
+		}
+		//export ljs
+		window.ljs = loader;
+		// eval inside tag code if any
+	}
+	script && gEval(script);
+})(window);
+
 (function($){
     $.fn.serializeObject = function(){
 
@@ -199,7 +427,6 @@ if (!Object.keys) {
 } 
 
 var initMura=function(config){
-	var hashtable={};
 
 	var createCookie=function(name,value,days) {
 		if (days) {
@@ -237,228 +464,6 @@ var initMura=function(config){
 	    }
 	   }
 	}
- 
-	var hash=function(string) {
-	  var RotateLeft=function(lValue, iShiftBits) {
-	    return (lValue<<iShiftBits) | (lValue>>>(32-iShiftBits));
-	  }
-
-	  var AddUnsigned=function(lX,lY) {
-	    var lX4,lY4,lX8,lY8,lResult;
-	    lX8 = (lX & 0x80000000);
-	    lY8 = (lY & 0x80000000);
-	    lX4 = (lX & 0x40000000);
-	    lY4 = (lY & 0x40000000);
-	    lResult = (lX & 0x3FFFFFFF)+(lY & 0x3FFFFFFF);
-	    if (lX4 & lY4) {
-	      return (lResult ^ 0x80000000 ^ lX8 ^ lY8);
-	    }
-	    if (lX4 | lY4) {
-	      if (lResult & 0x40000000) {
-	        return (lResult ^ 0xC0000000 ^ lX8 ^ lY8);
-	      } else {
-	        return (lResult ^ 0x40000000 ^ lX8 ^ lY8);
-	      }
-	    } else {
-	      return (lResult ^ lX8 ^ lY8);
-	    }
-	   }
-
-	   var F=function(x,y,z) { return (x & y) | ((~x) & z); }
-	   var G=function(x,y,z) { return (x & z) | (y & (~z)); }
-	   var H=function(x,y,z) { return (x ^ y ^ z); }
-	   var I=function(x,y,z) { return (y ^ (x | (~z))); }
-	 
-	   var FF=function(a,b,c,d,x,s,ac) {
-	    a = AddUnsigned(a, AddUnsigned(AddUnsigned(F(b, c, d), x), ac));
-	    return AddUnsigned(RotateLeft(a, s), b);
-	  };
-
-	  var GG=function(a,b,c,d,x,s,ac) {
-	    a = AddUnsigned(a, AddUnsigned(AddUnsigned(G(b, c, d), x), ac));
-	    return AddUnsigned(RotateLeft(a, s), b);
-	  };
-
-	  var HH=function(a,b,c,d,x,s,ac) {
-	    a = AddUnsigned(a, AddUnsigned(AddUnsigned(H(b, c, d), x), ac));
-	    return AddUnsigned(RotateLeft(a, s), b);
-	  };
-
-	  var II=function(a,b,c,d,x,s,ac) {
-	    a = AddUnsigned(a, AddUnsigned(AddUnsigned(I(b, c, d), x), ac));
-	    return AddUnsigned(RotateLeft(a, s), b);
-	  };
-
-	  var ConvertToWordArray=function(string) {
-	    var lWordCount;
-	    var lMessageLength = string.length;
-	    var lNumberOfWords_temp1=lMessageLength + 8;
-	    var lNumberOfWords_temp2=(lNumberOfWords_temp1-(lNumberOfWords_temp1 % 64))/64;
-	    var lNumberOfWords = (lNumberOfWords_temp2+1)*16;
-	    var lWordArray=Array(lNumberOfWords-1);
-	    var lBytePosition = 0;
-	    var lByteCount = 0;
-	    while ( lByteCount < lMessageLength ) {
-	      lWordCount = (lByteCount-(lByteCount % 4))/4;
-	      lBytePosition = (lByteCount % 4)*8;
-	      lWordArray[lWordCount] = (lWordArray[lWordCount] | (string.charCodeAt(lByteCount)<<lBytePosition));
-	      lByteCount++;
-	    }
-	    lWordCount = (lByteCount-(lByteCount % 4))/4;
-	    lBytePosition = (lByteCount % 4)*8;
-	    lWordArray[lWordCount] = lWordArray[lWordCount] | (0x80<<lBytePosition);
-	    lWordArray[lNumberOfWords-2] = lMessageLength<<3;
-	    lWordArray[lNumberOfWords-1] = lMessageLength>>>29;
-	    return lWordArray;
-	  };
-
-	  var WordToHex=function(lValue) {
-	    var WordToHexValue="",WordToHexValue_temp="",lByte,lCount;
-	    for (lCount = 0;lCount<=3;lCount++) {
-	      lByte = (lValue>>>(lCount*8)) & 255;
-	      WordToHexValue_temp = "0" + lByte.toString(16);
-	      WordToHexValue = WordToHexValue + WordToHexValue_temp.substr(WordToHexValue_temp.length-2,2);
-	    }
-	    return WordToHexValue;
-	  };
-
-	  var Utf8Encode=function(string) {
-	    string = string.replace(/\r\n/g,"\n");
-	    var utftext = "";
-
-	    for (var n = 0; n < string.length; n++) {
-	      var c = string.charCodeAt(n);
-
-	      if (c < 128) {
-	        utftext += String.fromCharCode(c);
-	      }
-	      else if((c > 127) && (c < 2048)) {
-	        utftext += String.fromCharCode((c >> 6) | 192);
-	        utftext += String.fromCharCode((c & 63) | 128);
-	      }
-	      else {
-	        utftext += String.fromCharCode((c >> 12) | 224);
-	        utftext += String.fromCharCode(((c >> 6) & 63) | 128);
-	        utftext += String.fromCharCode((c & 63) | 128);
-	      }
-	    }
-
-	    return utftext;
-	  };
-
-	  var x=Array();
-	  var k,AA,BB,CC,DD,a,b,c,d;
-	  var S11=7, S12=12, S13=17, S14=22;
-	  var S21=5, S22=9 , S23=14, S24=20;
-	  var S31=4, S32=11, S33=16, S34=23;
-	  var S41=6, S42=10, S43=15, S44=21;
-
-	  string = Utf8Encode(string);
-
-	  x = ConvertToWordArray(string);
-
-	  a = 0x67452301; b = 0xEFCDAB89; c = 0x98BADCFE; d = 0x10325476;
-
-	  for (k=0;k<x.length;k+=16) {
-	    AA=a; BB=b; CC=c; DD=d;
-	    a=FF(a,b,c,d,x[k+0], S11,0xD76AA478);
-	    d=FF(d,a,b,c,x[k+1], S12,0xE8C7B756);
-	    c=FF(c,d,a,b,x[k+2], S13,0x242070DB);
-	    b=FF(b,c,d,a,x[k+3], S14,0xC1BDCEEE);
-	    a=FF(a,b,c,d,x[k+4], S11,0xF57C0FAF);
-	    d=FF(d,a,b,c,x[k+5], S12,0x4787C62A);
-	    c=FF(c,d,a,b,x[k+6], S13,0xA8304613);
-	    b=FF(b,c,d,a,x[k+7], S14,0xFD469501);
-	    a=FF(a,b,c,d,x[k+8], S11,0x698098D8);
-	    d=FF(d,a,b,c,x[k+9], S12,0x8B44F7AF);
-	    c=FF(c,d,a,b,x[k+10],S13,0xFFFF5BB1);
-	    b=FF(b,c,d,a,x[k+11],S14,0x895CD7BE);
-	    a=FF(a,b,c,d,x[k+12],S11,0x6B901122);
-	    d=FF(d,a,b,c,x[k+13],S12,0xFD987193);
-	    c=FF(c,d,a,b,x[k+14],S13,0xA679438E);
-	    b=FF(b,c,d,a,x[k+15],S14,0x49B40821);
-	    a=GG(a,b,c,d,x[k+1], S21,0xF61E2562);
-	    d=GG(d,a,b,c,x[k+6], S22,0xC040B340);
-	    c=GG(c,d,a,b,x[k+11],S23,0x265E5A51);
-	    b=GG(b,c,d,a,x[k+0], S24,0xE9B6C7AA);
-	    a=GG(a,b,c,d,x[k+5], S21,0xD62F105D);
-	    d=GG(d,a,b,c,x[k+10],S22,0x2441453);
-	    c=GG(c,d,a,b,x[k+15],S23,0xD8A1E681);
-	    b=GG(b,c,d,a,x[k+4], S24,0xE7D3FBC8);
-	    a=GG(a,b,c,d,x[k+9], S21,0x21E1CDE6);
-	    d=GG(d,a,b,c,x[k+14],S22,0xC33707D6);
-	    c=GG(c,d,a,b,x[k+3], S23,0xF4D50D87);
-	    b=GG(b,c,d,a,x[k+8], S24,0x455A14ED);
-	    a=GG(a,b,c,d,x[k+13],S21,0xA9E3E905);
-	    d=GG(d,a,b,c,x[k+2], S22,0xFCEFA3F8);
-	    c=GG(c,d,a,b,x[k+7], S23,0x676F02D9);
-	    b=GG(b,c,d,a,x[k+12],S24,0x8D2A4C8A);
-	    a=HH(a,b,c,d,x[k+5], S31,0xFFFA3942);
-	    d=HH(d,a,b,c,x[k+8], S32,0x8771F681);
-	    c=HH(c,d,a,b,x[k+11],S33,0x6D9D6122);
-	    b=HH(b,c,d,a,x[k+14],S34,0xFDE5380C);
-	    a=HH(a,b,c,d,x[k+1], S31,0xA4BEEA44);
-	    d=HH(d,a,b,c,x[k+4], S32,0x4BDECFA9);
-	    c=HH(c,d,a,b,x[k+7], S33,0xF6BB4B60);
-	    b=HH(b,c,d,a,x[k+10],S34,0xBEBFBC70);
-	    a=HH(a,b,c,d,x[k+13],S31,0x289B7EC6);
-	    d=HH(d,a,b,c,x[k+0], S32,0xEAA127FA);
-	    c=HH(c,d,a,b,x[k+3], S33,0xD4EF3085);
-	    b=HH(b,c,d,a,x[k+6], S34,0x4881D05);
-	    a=HH(a,b,c,d,x[k+9], S31,0xD9D4D039);
-	    d=HH(d,a,b,c,x[k+12],S32,0xE6DB99E5);
-	    c=HH(c,d,a,b,x[k+15],S33,0x1FA27CF8);
-	    b=HH(b,c,d,a,x[k+2], S34,0xC4AC5665);
-	    a=II(a,b,c,d,x[k+0], S41,0xF4292244);
-	    d=II(d,a,b,c,x[k+7], S42,0x432AFF97);
-	    c=II(c,d,a,b,x[k+14],S43,0xAB9423A7);
-	    b=II(b,c,d,a,x[k+5], S44,0xFC93A039);
-	    a=II(a,b,c,d,x[k+12],S41,0x655B59C3);
-	    d=II(d,a,b,c,x[k+3], S42,0x8F0CCC92);
-	    c=II(c,d,a,b,x[k+10],S43,0xFFEFF47D);
-	    b=II(b,c,d,a,x[k+1], S44,0x85845DD1);
-	    a=II(a,b,c,d,x[k+8], S41,0x6FA87E4F);
-	    d=II(d,a,b,c,x[k+15],S42,0xFE2CE6E0);
-	    c=II(c,d,a,b,x[k+6], S43,0xA3014314);
-	    b=II(b,c,d,a,x[k+13],S44,0x4E0811A1);
-	    a=II(a,b,c,d,x[k+4], S41,0xF7537E82);
-	    d=II(d,a,b,c,x[k+11],S42,0xBD3AF235);
-	    c=II(c,d,a,b,x[k+2], S43,0x2AD7D2BB);
-	    b=II(b,c,d,a,x[k+9], S44,0xEB86D391);
-	    a=AddUnsigned(a,AA);
-	    b=AddUnsigned(b,BB);
-	    c=AddUnsigned(c,CC);
-	    d=AddUnsigned(d,DD);
-	  }
-
-	  var temp = WordToHex(a)+WordToHex(b)+WordToHex(c)+WordToHex(d);
-
-	  return temp.toLowerCase();
-	}
-
-	var getScript=function(script,callback){
-		var hashcode=hash(script);
-    	
-    	if(typeof hashtable[hashcode] == 'undefined'){
-			$.getScript(script, function(){
-    			hashtable[hashcode]=true;
-    			callback();
-    		}); 
-    	} else {
-    		callback();
-    	}
-	}
-
-	var getScripts=function(scripts, callback) {
-	    var progress = 0;
-	    var internalCallback = function () {
-	        if (++progress == scripts.length) { callback(); }
-	    };
-
-	    for(var s=0;s < scripts.length;s++){
-	    	getScript(scripts[s],internalCallback);
-	    }
-	};
 
 	var noSpam=function(user,domain) {
 		locationstring = "mailto:" + user + "@" + domain;
@@ -687,26 +692,6 @@ var initMura=function(config){
 				return true;}
 
 	}
-
-	var createUUID= function() {
-	    var s = [], itoh = '0123456789ABCDEF';
-	   
-	    // Make array of random hex digits. The UUID only has 32 digits in it, but we
-	    // allocate an extra items to make room for the '-'s we'll be inserting.
-	    for (var i = 0; i < 35; i++) s[i] = Math.floor(Math.random()*0x10);
-	   
-	    // Conform to RFC-4122, section 4.4
-	    s[14] = 4;  // Set 4 high bits of time_high field to version
-	    s[19] = (s[19] & 0x3) | 0x8;  // Specify 2 high bits of clock sequence
-	   
-	    // Convert to hex chars
-	    for (var i = 0; i < 36; i++) s[i] = itoh[s[i]];
-	   
-	    // Insert '-'s
-	    s[8] = s[13] = s[18] = '-';
-	   
-	    return s.join('');
-  	}
 
 	var validateForm=function(frm,customaction) {
 
@@ -999,6 +984,7 @@ var initMura=function(config){
 		return (obj);
 	}
 
+	var loader=function(){return window.ljs;}
 
 	if(!config.apiEndpoint){
 		config.apiEndpoint=config.context + '/index.cfm/_api/ajax/v1/';
@@ -1015,7 +1001,7 @@ var initMura=function(config){
 	    		$(self).html(resp.data.html);
 
 	 			if($(self).data('objectscript')){
-	 				getScript(
+	 				loader().loadjs(
 						$(self).data('objectscript'),
 						function(){
 							if($(self).data('objectinit')){
@@ -1038,11 +1024,11 @@ var initMura=function(config){
 				});
 
 				if($(self).find( ".ffp_mm" ).length){
-					getScript(config.context + '/requirements/cfformprotect/js/cffp.js');
+					loader().loadjs(config.context + '/requirements/cfformprotect/js/cffp.js');
 				}
 
 				if($(self).find( ".g-recaptcha" ).length){
-					getScript('https://www.google.com/recaptcha/api.js?hl=' + config.reCAPTCHALanguage);
+					loader().loadjs('https://www.google.com/recaptcha/api.js?hl=' + config.reCAPTCHALanguage);
 				}
 
 				$(self).find('form').each(function(){
@@ -1148,8 +1134,7 @@ var initMura=function(config){
 		processAsyncObject:processAsyncObject,
 		setLowerCaseKeys:setLowerCaseKeys,
 		noSpam:noSpam,
-		getScripts:getScripts,
-		getScript:getScript
+		loader:loader
 	});
 
 	$.extend(window,{
@@ -1173,3 +1158,4 @@ var initMura=function(config){
 	});
 
 };
+
