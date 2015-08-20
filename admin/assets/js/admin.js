29eff4949000508fbd17f175851e3be09020bf81
Mat Levine <matt.levine@blueriver.com>
2015-08-20 11:12:40 -0700

    Fixing js error working towards true query pagination

--- admin/assets/js/admin.js
+++ admin/assets/js/admin.js
@@ -582,7 +582,7 @@ function validateForm(theForm) {
 		}
 
 		$("#alertDialogMessage").html(errors);
-		("#alertDialog").attr('title','Alert');
+		$("#alertDialog").attr('title','Alert');
 		$("#alertDialog").dialog({
 			resizable: false,
 			modal: true,
