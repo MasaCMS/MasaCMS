d4f4641ef2c1eeab03f888a59410a7ca19e0ad3b
Mat Levine <matt.levine@blueriver.com>
2016-01-04 14:29:01 -0800

    Working on layout manager

--- default/js/src/mura.js
+++ default/js/src/mura.js
@@ -1241,7 +1241,7 @@
 
 	function loader(){return window.mura.ljs;}
 
-	var layoutmanagertoolbar='<div class="frontEndToolsModal mura"><i class="icon-pencil"></i>&nbsp;</div>';
+	var layoutmanagertoolbar='<div class="frontEndToolsModal mura"><i class="icon-pencil"></i></div>';
 
 	function processMarkup(scope){
 
