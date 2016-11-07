7927ab751dd89b6beb54735356029716d0810b59
Mat Levine <matt.levine@blueriver.com>
2016-11-06 21:50:39 -0800

    Updating Mura.js with new change from pull request

--- default/js/dist/mura.js
+++ default/js/dist/mura.js
@@ -4813,8 +4813,8 @@ return /******/ (function(modules) { // webpackBootstrap
 		obj=(obj.node) ? obj : Mura(obj);
 
 		// handle HTML response
-		resp=(!resp.data) ? { data: resp } : resp;
-		
+		resp=(!resp.data) ? { data: (typeof resp=='string') ? {html:resp}: resp } : resp;
+
 		if(typeof resp.data.redirect != 'undefined'){
 			if(resp.data.redirect && resp.data.redirect != location.href){
 				location.href=resp.data.redirect;
