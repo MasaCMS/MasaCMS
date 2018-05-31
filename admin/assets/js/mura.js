c29e892a07d01369afcb41f707d7d818d870ff39
Matt Levine <matt.levine@blueriver.com>
2018-05-31 10:51:10 -0700

    Fix for issue #2884

--- admin/assets/js/mura.js
+++ admin/assets/js/mura.js
@@ -1154,14 +1154,21 @@ var Mura=(function(){
    * @return {void}
    * @memberof {class} Mura
    */
-  function createCookie(name, value, days) {
-      if (days) {
-          var date = new Date();
-          date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-          var expires = "; expires=" + date.toGMTString();
-      } else var expires = "";
-      document.cookie = name + "=" + value + expires + "; path=/";
-  }
+	 function createCookie(name, value, days) {
+ 		if(days) {
+ 			var date = new Date();
+ 			date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+ 			var expires = "; expires=" + date.toGMTString();
+ 		} else {
+ 			var expires = "";
+ 		}
+ 		if(typeof location != 'undefined' && location.protocol == 'https:'){
+ 			secure='; secure';
+ 		} else {
+ 			secure='';
+ 		}
+ 		document.cookie = name + "=" + value + expires + "; path=/" + secure;
+ 	}
 
   /**
    * readCookie - Reads cookie value
