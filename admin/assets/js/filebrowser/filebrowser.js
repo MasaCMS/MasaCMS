40e80f3316adaf6ebdcc695c5c891be85c8e39dc
gowestweb <gowestweb@gmail.com>
2019-06-11 14:52:43 -0400

    Fixing filebrowser missing assets

--- admin/assets/js/filebrowser/filebrowser.js
+++ admin/assets/js/filebrowser/filebrowser.js
@@ -33,8 +33,8 @@ config: {
     .loadjs(
       Mura.adminpath + '/assets/js/vue.min.js',
       Mura.corepath + '/vendor/codemirror/codemirror.js',
-      Mura.corepath + '/vendor/codemirror/addon/formatting/formatting.js',
-      Mura.corepath + '/vendor/codemirror/mode/htmlmixed/htmlmixed.js',
+      // Mura.corepath + '/vendor/codemirror/addon/formatting/formatting.js',
+      // Mura.corepath + '/vendor/codemirror/mode/htmlmixed/htmlmixed.js',
     function() {
       self.mountbrowser();
      } ) ;
