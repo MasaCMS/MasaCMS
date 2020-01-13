feea4e9b49fe9c8b9fdc1921b0aaaf455db7e9b8
Matt Levine <matt.levine@blueriver.com>
2020-01-13 10:21:11 -0800

    Updating Mura.js

--- core/modules/v1/core_assets/js/mura.js
+++ core/modules/v1/core_assets/js/mura.js
@@ -2577,8 +2577,8 @@ var Mura=(function(){
 					} else if (lcaseObject=='form' || lcaseObject=='component'){
 
 						if(obj.data('perm')){
-							objectParams=obj.data();
-							if(window.MuraInlineEditor.objectHasConfigurator(obj) || (!window.Mura.layoutmanager && window.MuraInlineEditor.objectHasEditor(objectParams)) ){
+							var objectData=obj.data();
+							if(window.MuraInlineEditor.objectHasConfigurator(obj) || (!window.Mura.layoutmanager && window.MuraInlineEditor.objectHasEditor(objectData)) ){
 								obj.addClass('mura-active');
 								obj.hover(
 									Mura.initDraggableObject_hoverin,
@@ -2738,8 +2738,8 @@ var Mura=(function(){
 			}
 		}
 
-		var rendered = (rerender) ? false : obj.children('.mura-object-content').length
-		
+		var rendered = (rerender && !obj.data('async')) ? false : obj.children('.mura-object-content').length
+
 		queue = (queue == null || rendered) ? false : queue;
 		
 		if (document.createEvent && queue && !isScrolledIntoView(obj.node)) {
@@ -2785,12 +2785,14 @@ var Mura=(function(){
 			});
 		}
 
-		if (rendered) {
+		if (rendered && !obj.data('async')) {
+			
 			return new Promise(function(resolve, reject) {
 
 				obj.calculateDisplayObjectStyles();
 
 				if(!rerender && obj.data('render')=='client' && obj.children('.mura-object-content').length){
+					
 					var context=filterUnwantedParams(obj.data());
 					if(typeof context.instanceid != 'undefined' && typeof Mura.hydrationData[context.instanceid] != 'undefined'){
 						Mura.extend(context,Mura.hydrationData[context.instanceid]);
@@ -2837,7 +2839,7 @@ var Mura=(function(){
 						}
 					}
 				});
-
+	
 				if (typeof resolve == 'function') {
 					resolve(obj);
 				}
@@ -2846,6 +2848,7 @@ var Mura=(function(){
 		}
 	
 		return new Promise(function(resolve, reject) {
+			
 			var data = deepExtend(setLowerCaseKeys(obj.data()),
 				urlparams, {
 					siteid: Mura.siteid,
