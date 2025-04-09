987dc17bbbeab6da31a9bd28a388aa6a34a5deea
Jeroen Baartmans <83004496+jeroenweareorangeeu@users.noreply.github.com>
2025-04-09 15:02:19 +0200

    update password hash to BCrypt version (#115)

--- core/setup/db/h2.sql
+++ core/setup/db/h2.sql
@@ -553,7 +553,7 @@ CREATE TABLE tusers (
 
 <cfoutput>
 INSERT INTO tusers (UserID,GroupName,Fname,Lname,UserName,Password,PasswordCreated,Email,Company,JobTitle,mobilePhone,Website,Type,subType,Ext,ContactForm,Admin,S2,LastLogin,LastUpdate,LastUpdateBy,LastUpdateByID,Perm,InActive,isPublic,SiteID,Subscribe,notes,description,interests,keepPrivate,photoFileID,IMName,IMService,created,remoteID,tags) VALUES
- ('#adminUserID#',NULL,'Admin','User','admin','21232F297A57A5A743894A0E4A801FC3',now(),'admin@localhost.com',NULL,NULL,NULL,NULL,2,'Default',NULL,NULL,NULL,1,now(),now(),'System','22FC551F-FABE-EA01-C6EDD0885DDC1682',0,0,0,'default',0,NULL,NULL,NULL,0,NULL,NULL,NULL,now(),NULL,NULL),
+ ('#adminUserID#',NULL,'Admin','User','admin','$2a$10$9p0b3CVXN/UJwG1YSwZY8Oc1vbz8opCJMUr.9FNNl1BOXdkzTjE7e',now(),'admin@localhost.com',NULL,NULL,NULL,NULL,2,'Default',NULL,NULL,NULL,1,now(),now(),'System','22FC551F-FABE-EA01-C6EDD0885DDC1682',0,0,0,'default',0,NULL,NULL,NULL,0,NULL,NULL,NULL,now(),NULL,NULL),
  ('#createUUID()#','Admin',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,1,'Default',NULL,NULL,NULL,0,NULL,now(),'System',NULL,1,0,0,'default',0,NULL,NULL,NULL,0,NULL,NULL,NULL,now(),NULL,NULL);
 </cfoutput>
 
